3fcbbf1d0143f51bf58ff504cf177f77a9921359
Glitch (hello-webpage) <none>
2021-03-13 03:00:31 +0000

    ✋🚂 Checkpoint ./script.js:29680777/1089 ./index.html:29680777/442

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 var patter = [2,4,2,1,3,1,1,4];
 var progress = 0;
 var isPlayingGame = false;
+var tonePlaying = false;
+var volume = 0.5; 
 
 var startBtn = document.getElementById("startBtn");
 var stopBtn = document.getElementById("stopBtn");
@@ -22,3 +24,47 @@ const stopGame = () => {
 
 startBtn.addEventListener("click",startGame);
 stopBtn.addEventListener("click", stopGame);
+
+// Sound Synthesis Functions
+const freqMap = {
+  1: 261.6,
+  2: 329.6,
+  3: 392,
+  4: 466.2
+}
+function playTone(btn,len){ 
+  o.frequency.value = freqMap[btn]
+  g.gain.setTargetAtTime(volume,context.currentTime + 0.05,0.025)
+  tonePlaying = true
+  setTimeout(function(){
+    stopTone()
+  },len)
+}
+function startTone(btn){
+  if(!tonePlaying){
+    o.frequency.value = freqMap[1.25*btn]
+    g.gain.setTargetAtTime(volume,context.currentTime + 0.05,0.025)
+    tonePlaying = true
+  }
+}
+function stopTone(){
+    g.gain.setTargetAtTime(0,context.currentTime + 0.05,0.025)
+    tonePlaying = false
+}
+
+//Page Initialization
+// Init Sound Synthesizer
+var context = new AudioContext()
+var o = context.createOscillator()
+var g = context.createGain()
+g.connect(context.destination)
+g.gain.setValueAtTime(0,context.currentTime)
+o.connect(g)
+o.start(0)
+
+const addSound = (item, index) => {
+  item.addEventListerner("mousedown",)
+}
+
+const interactiveBtn = document.getElementsByClassName("playinButton");
+interactiveBtn.forEach()
