2272a2f5c40c9280c77fc45a8c4404c31d78a8a6
Glitch (hello-webpage) <none>
2021-03-13 02:49:30 +0000

    🕋🚊 Checkpoint ./style.css:29680777/113 ./script.js:29680777/1117

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+//Instance Variables
+var patter = [2,4,2,1,3,1,1,4];
+var progress = 0;
+var isPlayingGame = false;
 
-// prints "hi" in the browser's dev tools console
-console.log("hi");
+var startBtn = document.getElementById("startBtn");
+var stopBtn = document.getElementById("stopBtn");
+
+const startGame = () => {
+  isPlayingGame = true;
+  progress = 0;
+  document.getElementById("startBtn").classList.add("hidden");
+  document.getElementById("stopBtn").classList.remove("hidden");
+}
+
+const stopGame = () => {
+  isPlayingGame = false;
+  document.getElementById("stopBtn").classList.add("hidden");
+  document.getElementById("startBtn").classList.remove("hidden");
+}
+
+
+startBtn.addEventListener("click",startGame);
+stopBtn.addEventListener("click", stopGame);
